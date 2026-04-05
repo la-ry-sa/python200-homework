ec96399a4bb243c0a2b8a0dbe1de4bb5ae2cc3c9
Larysa Plostak <izumrud@gmail.com>
2026-04-05 01:08:01 -0700

    numpy warmup

--- assignments_01/warmup_01.py
+++ assignments_01/warmup_01.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # --- Pandas ---
 # Pandas Q1
@@ -42,3 +43,43 @@ print(f"Updated city names: \n{df[['name','city']]}")
 
 df_sorted = df.sort_values('grade', ascending = False)
 print(f"Top 3 students: \n{df_sorted.head(3)}")
+
+#--- NumPy ---
+# NumPy Q1
+
+arr = np.array([10, 20, 30, 40, 50])
+print(f"Array shape: {arr.shape}")
+print(f"Array data type: {arr.dtype}")
+print(f"Array ndim: {arr.ndim}")
+
+# NumPy Q2
+
+arr2 = np.array([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+print(f"Array shape: {arr2.shape}")
+print(f"Array size: {arr2.size}")
+
+# NumPy Q3
+
+print(f"Sliced array: \n{arr2[0:2, 0:2]}")
+
+#NumPy Q4
+
+print(f"2D array of zeros: \n{np.zeros((3, 4))}")
+print(f"2D array of ones: \n{np.ones((2, 5))}")
+
+# NumPy Q5
+
+arr3 = np.arange(0, 50, 5)
+print(f"Array: \n{arr3}")
+print(f"Array shape: {arr3.shape}")
+print(f"Array mean: {arr3.mean()}")
+print(f"Array sum: {arr3.sum()}")
+print(f"Array standard deviation: {arr3.std()}")
+
+# NumPy Q6
+
+arr4 = np.random.normal(0, 1, 200)
+print(f"Array mean: {arr4.mean()}")
+print(f"Array standard deviation: {arr4.std()}")
